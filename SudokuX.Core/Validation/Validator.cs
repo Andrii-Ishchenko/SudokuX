84e4731447f2c08ad9a6d67e309af6658a6fe258
Andrii-Ishchenko <Andrii_Ishchenko1@epam.com>
2017-11-08 01:41:07 +0200

    bootstrap for validation

---
+++ SudokuX.Core/Validation/Validator.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SudokuX.Core
+{
+    public class Validator
+    {
+        public ValidateResult ValidateSudoku(Sudoku sudoku)
+        { 
+            return new ValidateResult();
+        }
+
+        public ValidateResult ValidateGroup(Group g)
+        {
+            return new ValidateResult();
+        }
+        
+    }
+}
